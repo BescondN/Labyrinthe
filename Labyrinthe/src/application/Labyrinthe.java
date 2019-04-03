5d7feb591c6918bf1dd01349abf3bc5a6b17d3d4
alexandrefarret <alexandre.farret5@gmail.com>
2019-04-03 18:12:29 +0200

    Début classe

--- Labyrinthe/src/application/Labyrinthe.java
+++ Labyrinthe/src/application/Labyrinthe.java
@@ -1,7 +1,31 @@
 package application;
 
-public class Labyrinthe {
+public class Labyrinthe implements Explorable {
+	
+	private int labyrinthe[][];
+	private int startingBox;
+	private int arrivalBox;
+	private int adjacentBoxes[];
 	
 	
+	//Méthodes
+	
+	public Labyrinthe(String fichier) {
+		chargerFichier();
+	}
+	
+	public void chargerFichier() {
+		Path road = Paths.get("C")
+	}
 
+	//Créer Labyrinthe
+	//Cherche case départ
+	//Chercher case d'arrivée
+	//Charge fichier
+	//getCaseDépart
+	//getCaseArrivée
+	//getCaseVoisine
+	
+	
+	
 }
