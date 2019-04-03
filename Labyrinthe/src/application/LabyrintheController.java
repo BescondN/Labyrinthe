42a28031624e1d3048f3aeab4b52dc5a1a6222ec
Soukayna <Soukayna@HANDAS>
2019-04-03 19:39:40 +0200

    ajout des bouttons

--- Labyrinthe/src/application/LabyrintheController.java
+++ Labyrinthe/src/application/LabyrintheController.java
@@ -1,5 +1,17 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
 public class LabyrintheController {
+	
+	@FXML
+	private ComboBox Atelier;
+	
+	@FXML
+	private ComboBox Algo;
+	
+	
 	
 }
